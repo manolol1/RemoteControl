edaddcdc9e9d6c3e7b503034af52ba6fa268fe2b
manolol1 <manolol@mailbox.org>
2024-04-16 15:10:45 +0200

    Implement custom scripts support

--- client.js
+++ client.js
@@ -1,7 +1,15 @@
+// Settings
+const clientPort = 2052;
+
+const scriptsEnabled = false // DANGEROUS! Anyone will be able to execute all scripts in the specified scripts directory, if enabled!
+const scriptsPath = "./scripts"; // set the path to your scripts directory here!
+
+
 const express = require('express');
 const app = express();
 
-const clientPort = 2052;
+const fs = require('fs');
+const path = require('path');
 
 const shutdownCommand = "sudo shutdown -P now";
 
@@ -24,6 +32,45 @@ app.get('/ping', (req, res) => {
     res.status(200);
     res.send('Pong!');
 });
+
+// load all scripts
+if (scriptsEnabled) {
+    fs.readdir(scriptsPath, (err, files) => {
+        if (err) {
+            console.error(`Could not read scripts: ${err}`);
+    
+            app.get('/scripts/*', (req, res) => {
+                res.status(500);
+                res.send('Could not read scripts.');
+            });
+        }
+    
+        app.get('/scripts', (req, res) => {
+            res.status(200);
+            res.send("Available scripts: " + files.join(", "));
+        });
+    
+        // For each file, create a route that will execute the script when requested
+        files.forEach((file, index) => {
+            const filePath = path.join(scriptsPath, file);
+            app.get(`/scripts/${file}`, (req, res) => {
+                exec(`bash ${filePath}`, (error, stdout, stderr) => {
+                    if (error) {
+                        console.error(`exec error: ${error}`);
+                        return res.status(500).send('Script execution failed.');
+                    }
+                    // Send the output of the script back to the client
+                    return res.status(200).send(`Script executed.<br/><br/>stdout:<br/>${stdout}<br/><br/>stderr:<br/>${stderr}`);
+                });
+            });
+        });
+    });
+} else {
+    app.get('/scripts*', (req, res) => {
+        res.status(403);
+        res.send("Scripts are disabled.");
+    });
+}
 
 app.listen(clientPort, () => {
     console.log(`RemoteControl Client is listening on port ${clientPort}...`);
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+// Settings
+const serverPort = 2082
+const clientAddress = "example.com:2052";
+const wolMacAddress = 'a0:b1:c2:d3:e4:f5';
+
+
 const express = require('express')
 const app = express()
 
-const serverPort = 2082
-const clientAddress = "example.com:2052";
-
-const wolMacAddress = 'a0:b1:c2:d3:e4:f5';
-
 const http = require('http');
 const wol = require('wake_on_lan');
+const axios = require('axios');
 
 app.get('/wakeup', (req, res) => {
   wol.wake(wolMacAddress, function (error) {
@@ -40,7 +42,34 @@ app.get('/ping', (req, res) => {
     console.error(`Error during ping request: ${err.message}`);
     res.send('Ping request failed. The client is most likely offline.');
   });
+});
 
+app.get('/scripts', (req, res) => {
+  axios.get(`http://${clientAddress}/scripts`)
+    .then(response => {
+      const scripts = response.data.split(', ');
+
+      // For each script, create a route
+      scripts.forEach(script => {
+        app.get(`/scripts/${script}`, (req, res) => {
+          // Make a request to the client to execute the script
+          axios.get(`http://${clientAddress}/scripts/${script}`)
+            .then(response => {
+              res.send(response.data);
+            })
+            .catch(error => {
+              console.error(`Error during script execution: ${error.message}`);
+              res.send(`Script ${script} execution failed.`);
+            });
+        });
+      });
+
+      res.send(scripts.join(', '));
+    })
+    .catch(error => {
+      console.error(`Error during scripts request: ${error.message}`);
+      res.send('Scripts request failed.');
+    });
 });
 
 app.listen(serverPort, () => {
